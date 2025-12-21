73382e298c311a20b9db25cc8523b6a677478b15
Eric Mascot <ericmascot@google.com>
2025-12-21 03:33:11 -0800

    Hide unnecessary columns

--- examples/RISQTutorial/src/RISQTutorialSensitivity.cc
+++ examples/RISQTutorial/src/RISQTutorialSensitivity.cc
@@ -77,17 +77,17 @@ void RISQTutorialSensitivity::EndOfEvent(G4HCofThisEvent* HCE) {
                 << runMan->GetCurrentEvent()->GetEventID() << ","
                 << hit->GetTrackID() << ","
                 << q << ","
-                << hit->GetStartEnergy()/eV << ","
-                << hit->GetStartPosition().getX()/mm << ","
-                << hit->GetStartPosition().getY()/mm << ","
-                << hit->GetStartPosition().getZ()/mm << ","
-                << hit->GetStartTime()/ns << ","
-                << hit->GetEnergyDeposit()/eV << ","
-                << hit->GetWeight() << ","
-                << hit->GetFinalPosition().getX()/mm << ","
-                << hit->GetFinalPosition().getY()/mm << ","
-                << hit->GetFinalPosition().getZ()/mm << ","
-                << hit->GetFinalTime()/ns << "\n";
+                // << hit->GetStartEnergy() / eV << ","
+                // << hit->GetStartPosition().getX() / mm << ","
+                // << hit->GetStartPosition().getY() / mm << ","
+                // << hit->GetStartPosition().getZ() / mm << ","
+                // << hit->GetStartTime() / ns << ","
+                // << hit->GetEnergyDeposit() / eV << ","
+                // << hit->GetWeight() << ","
+                << hit->GetFinalPosition().getX() / mm << ","
+                << hit->GetFinalPosition().getY() / mm << ","
+                << hit->GetFinalPosition().getZ() / mm << ","
+                << hit->GetFinalTime() / ns << "\n";
     }
   }
 }
@@ -104,10 +104,21 @@ void RISQTutorialSensitivity::SetHitOutputFile(const G4String &fn) {
                   FatalException, msg);
       hitOutput.close();
     } else {
-      hitOutput << "Run ID,Event ID,Track ID,Charge [e],Start Energy [eV],"
-		<< "Start X [mm],Start Y [mm],Start Z [mm],Start Time [ns],"
-		<< "Energy Deposited [eV],Track Weight,End X [mm],End Y [mm],End Z [mm],"
-		<< "Final Time [ns]\n";
+      hitOutput << "Run ID,"
+                << "Event ID,"
+                << "Track ID,"
+                << "Charge [e],"
+                // << "Start Energy [eV],"
+                // << "Start X [mm],"
+                // << "Start Y [mm],"
+                // << "Start Z [mm],"
+                // << "Start Time [ns],"
+                // << "Energy Deposited [eV],"
+                // << "Track Weight,"
+                << "End X [mm],"
+                << "End Y [mm],"
+                << "End Z [mm],"
+                << "Final Time [ns]\n";
     }
   }
 }
